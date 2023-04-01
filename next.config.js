5dfb9c4a5ae18873c0298deb7959f97992549361
Alibek Kazhibekov <alibek.gao@gmail.com>
2023-04-01 22:20:00 +0200

    add remote page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'remote',
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './Page': './pages/index.js',
+        },
+        extraOptions: {
+          automaticAsyncBoundary: true,
+        },
+        shared: {},
+      })
+    );
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
